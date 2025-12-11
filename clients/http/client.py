0f48def88cb7ebaa90324bc9f34ec8155b50d41a
kapKurgan <kkaapp@yandex.ru>
2025-12-11 22:06:21 +0500

    Обновим HTTPClient для поддержки extensions

--- clients/http/client.py
+++ clients/http/client.py
@@ -1,5 +1,10 @@
 from httpx import Client, URL, QueryParams, Response
-from typing import Any
+from typing import Any, TypedDict
+
+
+class HTTPClientExtensions(TypedDict, total=False):
+    route: str
+
 
 class HTTPClient:
     """ Базовый HTTP API клиент, принимающий объект httpx.Client.
@@ -8,16 +13,18 @@ class HTTPClient:
     def __init__(self, client: Client):
         self.client = client
 
-    def get(self, url: URL | str, params: QueryParams | None = None) -> Response:       # 7.3.4
+    def get(self, url: URL | str, params: QueryParams | None = None, extensions: HTTPClientExtensions | None = None) -> Response:       # 7.3.4
         """ Выполняет GET-запрос.
             :param url: URL-адрес эндпоинта.
             :param params: GET-параметры запроса (например, ?key=value).
+            :param extensions: Дополнительные данные, передаваемые через HTTPX extensions.
             :return: Объект Response с данными ответа. """
-        return self.client.get(url, params=params)
+        return self.client.get(url, params=params, extensions=extensions)
 
-    def post(self, url: URL | str, json: Any | None = None) -> Response:
+    def post(self, url: URL | str, json: Any | None = None, extensions: HTTPClientExtensions | None = None) -> Response:
         """ Выполняет POST-запрос.
             :param url: URL-адрес эндпоинта.
             :param json: Данные в формате JSON.
+            :param extensions: Дополнительные данные, передаваемые через HTTPX extensions.
             :return: Объект Response с данными ответа. """
-        return self.client.post(url, json=json)
+        return self.client.post(url, json=json, extensions=extensions)
